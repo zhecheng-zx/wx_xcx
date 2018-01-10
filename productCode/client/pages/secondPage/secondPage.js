6c524128d673f6e43a12bac7758f37c01814195c
lifei <39952175@qq.com>
2018-01-10 13:35:16 +0800

    登录弹窗

--- productCode/client/pages/secondPage/secondPage.js
+++ productCode/client/pages/secondPage/secondPage.js
@@ -65,9 +65,17 @@ Page({
     selectYear: '',
     YIWANCHENG:10,
     LIXIAGl:0,
-    wanyuan: 20
+    wanyuan: 20,
+    hiddenmodalput: false, //onReady里写方法判断是否登录，未登录：false，已登录：true
   },
+  //弹窗的重置按钮点击事件
+  resetClick: function () {
 
+  },
+  //弹窗的确认按钮点击事件
+  confirmClick: function(){
+
+  }, 
 /**
  * 开始日期改变事件
  */
@@ -75,7 +83,6 @@ Page({
     this.setData({
       beginDate: e.detail.value
     });
-    this.loadAggrProposalCount();
   },
 
   /**
@@ -98,12 +105,48 @@ Page({
     that.panel2Data();
   },
 
-
+  /**
+   * 
+   * 选择年份改变事件
+   */
+  yearChange: function(e){
+    var year = e.detail.value;
+    this.setData({
+      selectYear: year
+    });
+    this.panel1Data();
+  },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+  },
+  panel1Data:function(){
+    var that = this;
+    wx.request({
+      url: `${config.service.secondPageUrl6}`,
+      data: { stat_year: '' + that.data.selectYear },
+      header: {
+        "Content-Type": "application/json"
+      },
+      success(result) {
+        util.showSuccess('请求成功完成')
+        var results = result.data;
+        var tableData = results.data;
+        if (results.status != 1 && results.data) {
+          that.setData({
+            YIWANCHENG: tableData[0].count,
+            LIXIAGl: tableData[2].count,
+            wanyuan: tableData[1].count
+          });
+        }
+      },
+      fail(error) {
+        util.showModel('请求失败', error);
+        console.log('request fail', error);
+      }
+    });
   },
   panel2Data: function () {
     util.showBusy('请求中...');
@@ -364,12 +407,11 @@ Page({
       beginDate: startDate,
       selectYear: year
     });
-    //that.panel1Data();
+    that.panel1Data();
     that.panel2Data();
     // ringChart.addEventListener('renderComplete', () => {
     //   console.log('renderComplete');
-    // });
-    
+    // });    
   },
 
   /**
